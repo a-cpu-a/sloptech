a7465dd6480373ba6946bcb6b91d13f89ce2efe4
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2026-02-17 16:15:58 +0200

    Improved pipe and wrench behaviour Connect/disconnect both sides, not just the selected one. Give 9x/4x pipes their texture Register cables, wires

--- content/pipey/init.lua
+++ content/pipey/init.lua
@@ -76,7 +76,30 @@ local VENT_TIERS = {
 
 local b = '0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-local handleRclick;
+local LUT = {}
+local function buildPipeyBlockName(info)
+    local suf = "";
+    if #info.type ~= 0 then
+        suf = info.type .. '_'
+    end
+    --print(mn .. ":" .. info.kind .. "_" .. info.tier .. "_" .. suf .. info.shapeId)
+    return mn .. ":" .. info.kind .. "_" .. info.tier .. "_" .. suf .. info.shapeId
+end
+local function updatePipeyBlockName(oldInfo, newShapeId)
+    return buildPipeyBlockName({
+        kind = oldInfo.kind,
+        type = oldInfo.type,
+        tier = oldInfo.tier,
+        shapeId = newShapeId
+    });
+end
+local function shapeNum2Id(num)
+    if num == 64 then
+        return '00'
+    end
+    return b:sub(num, num)
+end
+
 local function regPipeyBlocks(kind, tierName, tierInfo)
     for shape = 1, 64 do -- Skip 0 (usually invisible/air)
         -- Box list, with the center
@@ -121,11 +144,15 @@ local function regPipeyBlocks(kind, tierName, tierInfo)
             end
         end
 
-        local shapeId = '00';
-        if shape ~= 64 then shapeId = b:sub(shape, shape) end
+        local shapeId = shapeNum2Id(shape);
 
-        -- 2. Register the Node
-        local name = mn .. ":" .. kind .. "_" .. tierName .. "_" .. shapeId
+        local info = { kind = kind, tier = tierName, type = "", shapeId = shapeId };
+        local name = buildPipeyBlockName(info);
+        LUT[name] = info;
+
+
+        local tiertex = "";
+        if tierName == 'quadruple' or tierName == 'nonuple' then tiertex = '_' .. tierName end
 
         core.register_node(name, {
             description = "Someium " .. kind .. "\n⁂₂\n"
@@ -136,8 +163,8 @@ local function regPipeyBlocks(kind, tierName, tierInfo)
                 "Transfer rate: 69L/t\nTemperature limit: 6900K\nCan handle gases, acids, cryogenics, all plasmas\nHAZARDOUS:\nCarcinogenic, caused by any contact",
             drawtype = "nodebox",
             paramtype = "light",
-            paramtype2 = "color",                                                     -- CRITICAL: Stores the material color
-            tiles = { mn .. "_blocks." .. kind .. "_base.png" .. "^[multiply:#FFF" }, -- Base texture
+            paramtype2 = "color",                                                                -- CRITICAL: Stores the material color
+            tiles = { mn .. "_blocks." .. kind .. tiertex .. "_base.png" .. "^[multiply:#FFF" }, -- Base texture
 
             -- We use color to tint the texture
             color = "#FFFFFF",
@@ -155,8 +182,13 @@ local function regPipeyBlocks(kind, tierName, tierInfo)
 
             drop = mn .. ":" .. kind .. "_" .. tierName .. "_B", -- Drop the base item
 
-            -- Logic to toggle connection with Wrench
-            on_rightclick = handleRclick
+            on_place = function(itemstack, placer, pointedThing)
+                local info = LUT[itemstack:get_name()];
+                local tmp = ItemStack(itemstack);
+                tmp:set_name(updatePipeyBlockName(info, '0'));
+                core.item_place(tmp, placer, pointedThing, math.random() * 256);
+                itemstack:set_count(itemstack:get_count() - 1);
+            end
         })
     end
 end
@@ -177,33 +209,46 @@ local function getDirIdx(dir)
     end
     return nil
 end
+local function opDirIdx(dir)
+    if dir == 0 then
+        return 5 -- Top
+    end
+    if dir == 5 then
+        return 0 -- Bottom
+    end
+    if dir == 1 then
+        return 4 -- Right
+    end
+    if dir == 4 then
+        return 1 -- Left
+    end
+    if dir == 2 then
+        return 3 -- Front
+    end
+    return 2     -- Back
+end
+local function areKindsCompatible(k1, k2)
+    if k1 == k2 then return true end
+    if k1 == 'wire' or k1 == 'cable' then
+        return k2 == 'wire' or k2 == 'cable'
+    end
+    return false;
+end
 
-local function mapNodeId2ShapeNum(id)
-    local suffix = id:sub(-2)
-
-    if suffix == "00" then
+local function mapNodeId2ShapeNum(shapeId)
+    if shapeId == "00" then
         return 64
     end
-
-    local last = suffix:sub(-1)
-
-    --print('mapNodeId2ShapeNum, noden: ' ..
-    --    id .. ' suffix: ' .. suffix .. ' last: ' .. last)
-
     for i = 1, #b - 1 do -- -1 cuz no need to check last one
-        if last == b:sub(i, i) then
+        if shapeId == b:sub(i, i) then
             return i
         end
     end
     return 63
 end
 
-handleRclick = function(pos, node, clicker, itemstack, pointed_thing)
-    if itemstack:get_name() ~= "sloptech:test_wrench" then
-        return
-    end
-
-    local dir = vector.subtract(pointed_thing.above, pointed_thing.under)
+sloptech.wrenchUse = function(pos, posAbove)
+    local dir = vector.subtract(posAbove, pos)
     local idx = getDirIdx(dir)
 
     if not idx then -- Clicked from inside?
@@ -211,29 +256,41 @@ handleRclick = function(pos, node, clicker, itemstack, pointed_thing)
         return
     end
 
-    local shapeNum = mapNodeId2ShapeNum(node.name);
-    local bits = shapeNum - 1
-    bits = bit.bxor(bits, 2 ^ idx) -- Toggle the bit at index 'idx'
-    local newShape = bits + 1
-
-    local newShapeId = "00"
-    if newShape ~= 64 then
-        newShapeId = b:sub(newShape, newShape)
-    end
-
-    local was64 = 0;
-    if shapeNum == 64 then was64 = 1 end
-    local new = node.name:sub(1, -2 - was64) .. newShapeId
+    local node = core.get_node(pos);
+    local nodeInfo = LUT[node.name];
+    local shapeNum = mapNodeId2ShapeNum(nodeInfo.shapeId);
+    local newShape = bit.bxor(shapeNum - 1, 2 ^ idx) + 1
+    local connecting = bit.band(shapeNum - 1, 2 ^ idx) == 0;
+    local shapeId = shapeNum2Id(newShape);
+    local new = updatePipeyBlockName(nodeInfo, shapeId)
+    core.swap_node(pos, { name = new, param2 = node.param2 })
 
     --print('hi, noden: ' ..
     --    node.name .. ' fidx: ' .. idx .. ' sn: ' .. shapeNum .. ' news: ' .. newShape .. ' newi: ' .. new)
 
-    core.swap_node(pos, { name = new, param2 = node.param2 })
+    local p2 = vector.add(pos, dir);
+    local n2 = core.get_node(p2);
+    local n2Info = LUT[n2.name];
+    if n2Info ~= nil and areKindsCompatible(n2Info.kind, nodeInfo.kind) then
+        idx = opDirIdx(idx);
+        shapeNum = mapNodeId2ShapeNum(n2Info.shapeId);
+        local alreadyConnected = bit.band(shapeNum - 1, 2 ^ idx) ~= 0;
+        if alreadyConnected ~= connecting then
+            -- if user has odd mismatching ones, then match them together
+            newShape = bit.bxor(shapeNum - 1, 2 ^ idx) + 1
+            shapeId = shapeNum2Id(newShape);
+            new = updatePipeyBlockName(n2Info, shapeId)
+            core.swap_node(p2, { name = new, param2 = node.param2 })
+        end
+    end
 end
-
-
-
 
 for k, def in pairs(PIPE_TIERS) do
     regPipeyBlocks('pipe', k, def);
+end
+for k, def in pairs(WIRE_TIERS) do
+    regPipeyBlocks('wire', k, def);
+end
+for k, def in pairs(CABLE_TIERS) do
+    regPipeyBlocks('cable', k, def);
 end
--- content/tools/wrench.lua
+++ content/tools/wrench.lua
@@ -1,4 +1,10 @@
 core.register_tool("sloptech:test_wrench", {
     description = "Test Wrench\nFe₄S₂A₀\nFire, hello world", --208N
-    inventory_image = "sloptech_items.test_wrench.png"
+    inventory_image = "sloptech_items.test_wrench.png",
+    on_place = function(item, _p, pointedThing)
+        if pointedThing.type == "node" then
+            sloptech.wrenchUse(pointedThing.under, pointedThing.above);
+        end
+        return nil
+    end
 })
